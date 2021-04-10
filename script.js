5f39e96205b2b34e627527a0c9ecadc5fc94e9dc
uliana fomina <6758599@gmail.com>
2021-04-10 21:25:59 +0300

    adaptive version

--- script.js
+++ script.js
@@ -182,5 +182,21 @@ for(i=0;i<acc.length; i++){
     this.previousElementSibling.classList.add("svg-active");
   }
 }
+//-------------------------------------dropdown--------------------------------------//
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
 
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
